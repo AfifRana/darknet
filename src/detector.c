70e3698c8abbeef88fc89ea05ac60d2eaf56c6a5
Afif Rana Muhammad <36437450+AfifRana@users.noreply.github.com>
2021-06-26 11:24:35 +0700

    Fix error detector.c

--- src/detector.c
+++ src/detector.c
@@ -494,7 +494,7 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
 				sprintf(buff, "    [ New best mAP! ]");
 				fprintf(logfp, buff);
 				fclose(logfp);
-                char buff[256];
+                
                 sprintf(buff, "%s/%s_best.weights", backup_directory, base);
                 save_weights(net, buff);
             }
